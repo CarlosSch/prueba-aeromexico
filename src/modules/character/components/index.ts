fe50ff002adbd693fdb3ef80ceb577ee0912a688
Carlos Alberto López Garcés <stractovariuz@gmail.com>
2025-11-12 08:27:55 -0600

    feat: add CharacterGrid component

--- src/modules/character/components/index.ts
+++ src/modules/character/components/index.ts
@@ -1,3 +1,4 @@
 export * from './characterCard/CharacterCard';
 export * from './characterDetail/CharacterDetail';
+export * from './characterGrid/CharacterGrid';
 export * from './favoriteList/FavoriteList';
