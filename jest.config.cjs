a930171a2865789a968f9700699c4dde759f3432
Carlos Alberto López Garcés <stractovariuz@gmail.com>
2025-11-12 08:35:44 -0600

    chore: configure Jest mocks for files and styles

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   moduleNameMapper: {
+    '^@/(.*)\\.(png|jpg|jpeg|gif|webp|svg)$': '<rootDir>/src/tests/mocks/fileMock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': '<rootDir>/src/tests/mocks/styleMock.js',
+    '\\.(png|jpg|jpeg|gif|webp|svg)$': '<rootDir>/src/tests/mocks/fileMock.js',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
   },
   passWithNoTests: true,
 };
