dad8f68148bb1e24d299932a3d948555593b2800
Carlos Alberto López Garcés <stractovariuz@gmail.com>
2025-11-12 07:54:57 -0600

    feat: add delete icon

--- src/core/components/ui/icons/index.ts
+++ src/core/components/ui/icons/index.ts
@@ -1,5 +1,6 @@
 export * from './ArrowDownIcon';
 export * from './ArrowUpIcon';
+export * from './DeleteIcon';
 export * from './FavoriteIcon';
 export * from './SearchIcon';
 export * from './types';
